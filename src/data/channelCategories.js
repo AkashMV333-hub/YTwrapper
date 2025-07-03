dc4dba5575b3839bd45bc51d6c5dcb93f571af1f
AkashMV333-hub <mvakash333@gmail.com>
2025-07-03 13:22:10 +0530

    added channels

--- src/data/channelCategories.js
+++ src/data/channelCategories.js
@@ -1,6 +1,6 @@
 // src/data/channelCategories.js
 export const channelCategories = {
-  Travel: ['UCIsSPTBMJXYL-JtVAr1DcFg','UCLig0mTH2jvEpJsOtrZkvzA','UChsNu2nh9D1QiIi5ZFkyhIw'],
+  Travel: ['UCIsSPTBMJXYL-JtVAr1DcFg','UCLig0mTH2jvEpJsOtrZkvzA','UChsNu2nh9D1QiIi5ZFkyhIw','UCwfT8QeIiEaNmCIyOT4drJg','UCVTolKgdSnvi9hsMPrM6glw','UCu2D8oEJ34Z6XyS82ruupmw','UCLr1s9zKvByfxUTGk0egUug','UCAJEfWoif_AEikRsULXu5pA'],
   Movies: ['UCUinnqDgIsNFleLdkZKbP-w','UCWkHlUiYLeRSIMP8yM7rxxg','UCVbNbVDDZReLcDzfUWGTDtg','UCPWqbr91cfbtLHZrHM5GItg'],
   Finance: ['UCnHwry_kfr6oJ3W3nMpV6Nw','UCnHwry_kfr6oJ3W3nMpV6Nw']
 };
