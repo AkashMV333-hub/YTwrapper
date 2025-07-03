6ca51cf02e9f836e6a7c936907fc23664dfccd3d
AkashMV333-hub <mvakash333@gmail.com>
2025-07-03 08:52:01 +0530

    added channels

--- src/data/channelCategories.js
+++ src/data/channelCategories.js
@@ -1,5 +1,5 @@
 // src/data/channelCategories.js
 export const channelCategories = {
-  Travel: ['UCIsSPTBMJXYL-JtVAr1DcFg','UCLig0mTH2jvEpJsOtrZkvzA'],
-  Movies: ['UCUinnqDgIsNFleLdkZKbP-w'],
+  Travel: ['UCIsSPTBMJXYL-JtVAr1DcFg','UCLig0mTH2jvEpJsOtrZkvzA','UChsNu2nh9D1QiIi5ZFkyhIw'],
+  Movies: ['UCUinnqDgIsNFleLdkZKbP-w','UCWkHlUiYLeRSIMP8yM7rxxg','UCVbNbVDDZReLcDzfUWGTDtg','UCPWqbr91cfbtLHZrHM5GItg'],
 };
